827fc72561d3b85b78317e798d910f588fcfbf26
Jose Manuel <soldadojimenezj@gmail.com>
2024-10-03 23:21:43 +0200

    Añadiendo hola mundo con java y el uso de variables

---
+++ Programacion/Código Java/Hola Mundo/Holamundo.java
@@ -0,0 +1,6 @@
+public class Holamundo { // Java define clase llamada Holamundo(fichero), que es la estructura básica de un programa en Java.
+    public static void main(String[] args) { //Método principal o "main method", que es el punto de entrada del programa
+        
+        System.out.println("Hola mundo");
+    }
+}
