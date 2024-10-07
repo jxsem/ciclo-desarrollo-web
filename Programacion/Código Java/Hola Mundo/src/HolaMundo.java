265abac2a8d64dbbb6c797398b7695a24f590af2
Jose Manuel <soldadojimenezj@gmail.com>
2024-10-07 17:33:35 +0200

    Añadiendo Codigo Java con IntelliJ

---
+++ Programacion/Código Java/Hola Mundo/src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo { /*La primera clase publica, el nombre debe tener este formato NombreNombre*/
+    public static void main(String[] args) { /*Es un metodo publico que se puede llamar o ejecutar desde cualquier parte de la aplicación */
+        System.out.println("Hola Mundo desde Java"); /*Y por fin podemos imprimir algun valor por pantalla, en este caso damos la bienvenida al nuevo lenguaje*/
+    }
+}
+/*Para ejecutar o compilar (mejor dicho) el codigo, solo basta con darle click derecho en intellij*/
