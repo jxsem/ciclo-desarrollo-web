102acaf91b93ed12319a1e811fe154311f90808c
Jose Manuel <soldadojimenezj@gmail.com>
2024-10-07 18:04:08 +0200

    Añadiendo apuntes sobre las variables en Java

--- Programacion/Código Java/Hola Mundo/src/HolaMundo.java
+++ Programacion/Código Java/Hola Mundo/src/HolaMundo.java
@@ -1,6 +1,8 @@
 public class HolaMundo { /*La primera clase publica, el nombre debe tener este formato NombreNombre*/
-    public static void main(String[] args) { /*Es un metodo publico que se puede llamar o ejecutar desde cualquier parte de la aplicación */
-        System.out.println("Hola Mundo desde Java"); /*Y por fin podemos imprimir algun valor por pantalla, en este caso damos la bienvenida al nuevo lenguaje*/
+    public static void main(String[] args) {  /*Es un metodo publico que se puede llamar o ejecutar desde cualquier parte de la aplicación */
+
+        String saludar = "Hola mundo desde Java";
+        System.out.println(saludar);
     }
 }
 /*Para ejecutar o compilar (mejor dicho) el codigo, solo basta con darle click derecho en intellij*/
